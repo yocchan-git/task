cf4b7a771f000c3582d947039b5a79065747bffb
yoshi-gid <yoshipanman1201@gmail.com>
2023-08-28 14:32:49 +0900

    Create complete.php

---
+++ auth/complete.php
@@ -0,0 +1,14 @@
+<?php
+require('../components/header.php');
+?>
+<div style="text-align:center;" id="wrap">
+  <div id="head">
+    <h1>会員登録</h1>
+  </div>
+  <div id="content">
+		<p>ユーザー登録が完了しました</p>
+		<p><a href="login.php">ログインする</a></p>
+  </div>
+
+</div>
+<?php require('../components/footer.php'); ?>
