d3e127bbfba2ee4a7bf79bfbf3331ccf866af505
yoshi-gid <yoshipanman1201@gmail.com>
2023-08-28 14:33:04 +0900

    Create show.php

---
+++ project/show.php
@@ -0,0 +1,127 @@
+<?php
+require('../db/dbconnect.php');
+session_start();
+
+require('../auth/login-check.php');
+?>
+<?php
+$users = $db->prepare('SELECT * from users where id=?');
+$users->execute(array(
+    $_SESSION['id']
+));
+$user = $users->fetch();
+$id = $_GET['id'];
+
+$projects = $db->prepare('SELECT * from projects where id=?');
+$projects->execute(array(
+    $id
+));
+$project = $projects->fetch();
+
+$color = '';
+if($project['color_type'] == 'pink'){
+    $color = '#FEEEED';
+}elseif($project['color_type'] == 'sky'){
+    $color = '#F0F8FF';
+}else{
+    $color = 'white';
+}
+?>
+
+<?php
+// require('task/store.php');
+$i =  1;
+if(!empty($_POST)){
+    if($_POST['name'] == ''){
+        $error['name'] = 'blank';
+    }
+    if(empty($error)){
+       $statement = $db->prepare('INSERT into tasks set project_id=?, title=?,description=?,order_num=?,status=?,created_at=Now()');
+
+        echo $show = $statement->execute(array(
+            $id,
+            $_POST['name'],
+            $_POST['content'],
+            $i,
+            $_POST['states']
+        ));
+        unset($_POST);
+        // header('Location:index.php');
+        // exit();
+    }
+}
+$tasks = $db->prepare('SELECT * from tasks where project_id=?');
+$tasks->execute(array(
+    $id
+));
+?>
+
+<?php
+require('../components/header.php');
+// require('Project.php');
+?>
+<main>
+<a href="index.php">&laquo;&nbsp;戻る</a>
+<h2><?php echo htmlspecialchars($project['name'],ENT_QUOTES); ?></h2>
+<div class="show-flex">
+    <div class="show-content show-border">
+        <h3>未対応</h3>
+        <div id="show-open" onclick="showModalchange('showModal');">
+            <p class="show-plus">+ タスクを追加</p>
+        </div>
+        <div class="showModal" id="showModal">
+            <form action="" method="post">
+                <input type="hidden" name="states" value="will">
+                <label for="name">タイトル</label><br>
+                <input type="text" id="name" name="name"><br>
+
+                <label for="content">詳細</label><br>
+                <textarea name="content" id="content" cols="30" rows="5"></textarea><br>
+
+                <div class="text-aligin">
+                    <button>削除</button>
+                    <input type="submit" value="登録">
+                </div>
+            </form>
+        </div>
+
+        <?php foreach($tasks as $task): ?>
+        <div id="task<?php echo htmlspecialchars($task['id'],ENT_QUOTES); ?>" class="card" draggable="true">
+            <div>
+                <label style="margin-top:15px;">タイトル</label>
+                <p style="margin-top:5px;margin-bottom:10px;"><?php echo htmlspecialchars($task['title'],ENT_QUOTES); ?></p>
+            </div>
+            
+            <div>
+                <label style="margin:0;padding:0;">詳細</label>
+                <p style="margin-top:5px;margin-bottom:10px;"><?php echo htmlspecialchars($task['description'],ENT_QUOTES); ?></p>
+            </div>
+        
+            <div>
+                <button>削除</button>
+            </div>
+            
+            <!-- <form action="">
+                <input type="submit" value="削除"></button>
+            </form> -->
+        </div>
+        <?php endforeach; ?>
+    </div>
+    <div class="show-content show-border">
+        <h3>対応中</h3>
+    </div>
+
+    <div class="show-content show-border">
+        <h3>完了</h3>
+    </div>
+</div>
+</main>
+
+<style>
+    .show-color{
+        background-color:<?php echo $color; ?>;
+    }
+</style>
+
+<!-- <script src="https://unpkg.com/axios/dist/axios.min.js"><script> -->
+<?php require('../components/footer.php'); ?>
