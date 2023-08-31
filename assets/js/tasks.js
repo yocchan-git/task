c6028a5e516d561825c9dc7269d5baef90befffa
yoshi-gid <yoshipanman1201@gmail.com>
2023-08-31 09:36:54 +0900

    task関連のAjax処理

--- assets/js/tasks.js
+++ assets/js/tasks.js
@@ -1,28 +1,43 @@
+// 
 function accessPhpFile() {
-  let url = 'http://localhost/tasks/project/show.php';
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = () => {
-      if ( xhr.readyState === 4 ) {
-          if( xhr.status == 200) {
-              let taskname = document.getElementById('name').value;
-              let taskcontent = document.getElementById('content').value;
-              let id = document.getElementById('id').value;
-              let status = document.getElementById('status').value;
-              window.location.href = 'http://localhost/tasks/project/task/store.php?id='+id+'&title='+taskname+'&discription='+taskcontent+'&status='+status;
-              //通信成功時の処理
-          } else {
-            window.alert('通信失敗やで');
-              //通信失敗時の処理
-          }
-          //通信の成否に関わらず実行する処理
-      }
+  let taskname = document.getElementById('name').value;
+  let taskcontent = document.getElementById('content').value;
+  let id = document.getElementById('id').value;
+  let status = document.getElementById('status').value;
+  // データベースに追加したい内容を入れる
+  var dataToSend = {
+    project_id: id,
+    title: taskname,
+    discription: taskcontent,
+    status: status
   };
-  xhr.open( 'GET', url );
-  xhr.send(null);
 
+  var xhr = new XMLHttpRequest();
+
+  xhr.open("POST", "../project/task/store.php", true);
+  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhr.send(JSON.stringify(dataToSend));
   
+  xhr.onreadystatechange = () => {
+    if ( xhr.readyState === 4 ) {
+        if( xhr.status == 200) {
+          // 次はここにタスク追加のカードを閉じて欲しい
+          closeNewTask('showModal');
+          // カードを表示するファンクションを実行する
+          var response = xhr.responseText;
+          document.getElementById("new-content").innerHTML = response;
+            //通信成功時の処理
+        } else {
+          window.alert('通信失敗やで');
+            //通信失敗時の処理
+        }
+        //通信の成否に関わらず実行する処理
+    }
+  };
 
-  // postを使う方法
-  // xhr.open('GET', '../project/task/store.php');
-  // xhr.send();
+  xhr.open("POST", "../project/task/update.php", true);
+  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhr.send(JSON.stringify(dataToSend));
 }
+
+// 削除ボタンを押せばタスクカードが消えるファンクションを書く
