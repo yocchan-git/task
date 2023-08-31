a4c79b0f08ddd224cbc7a5ea849a1b3ae057d579
yoshi-gid <yoshipanman1201@gmail.com>
2023-08-31 18:08:45 +0900

    Update dbconnect.php

--- db/dbconnect.php
+++ db/dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 try{
-    $db = new PDO('mysql:dbname=kanri;host=localhost;charset=utf8','root','root');
+    $db = new PDO('mysql:dbname=rkmcl_tasks;host=mysql93.conoha.ne.jp;charset=utf8mb4','rkmcl_tasks','task&2525');
 }catch(PDOException $e){
     echo 'DB接続エラー:'.$e->getMessage();
 }
