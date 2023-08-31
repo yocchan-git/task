876ce9cf5d3848be778357f6eb4d17c2bd0f3a28
yoshi-gid <yoshipanman1201@gmail.com>
2023-08-31 09:35:05 +0900

    Ajaxで登録したら、カードをhtmlに表示する

--- project/task/update.php
+++ project/task/update.php
@@ -1,3 +1,27 @@
-<!-- 動かしたカードのidを取得する -->
+<!-- データを受け取り、カードの形にして返す -->
+<?php
 
-<!-- 動かしたカードの情報を更新する -->
+$data = json_decode(file_get_contents("php://input"), true);
+
+$test = $data['project_id'];
+$title = $data['title'];
+$discription = $data['discription'];
+$order_num = 1;
+$status = $data['status'];
+
+?>
+<div id="task<?php echo htmlspecialchars($test,ENT_QUOTES); ?>" class="card" draggable="true">
+            <div>
+                <label style="margin-top:15px;">タイトル</label>
+                <p style="margin-top:5px;margin-bottom:10px;"><?php echo htmlspecialchars($title,ENT_QUOTES); ?></p>
+            </div>
+            
+            <div>
+                <label style="margin:0;padding:0;">詳細</label>
+                <p style="margin-top:5px;margin-bottom:10px;"><?php echo htmlspecialchars($discription,ENT_QUOTES); ?></p>
+            </div>
+        
+            <div>
+                <button>削除</button>
+            </div>
+        </div>
