56b3ce2b37d368d852fd366b76e602e2c2ca60a5
yoshi-gid <yoshipanman1201@gmail.com>
2023-09-02 13:53:52 +0900

    タスクを削除する機能

--- project/task/delete.php
+++ project/task/delete.php
@@ -1,11 +1,8 @@
 <?php
 require('./Task.php');
-$data = json_decode(file_get_contents("php://input"), true);
 
-$id = $data['task_id'];
-$obj=new connect();
-
-$sql = 'DELETE from tasks where id=:id';
-$delete = $obj->delete($sql,$id);
-exit();
+if(!empty($_POST)){
+    $taskClass = new Task();
+    echo $taskClass->delete($_POST['id']);
+}
 ?>
