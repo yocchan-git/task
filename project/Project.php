1bc617fb4a376e40289b1f14ec8df923110294f8
yoshi-gid <yoshipanman1201@gmail.com>
2023-09-02 13:53:28 +0900

    プロジェクト関連のクラスを追加

--- project/Project.php
+++ project/Project.php
@@ -1,40 +1,45 @@
 <?php
-class connectProject {
-  //定数の宣言
-  const DB_NAME='rkmcl_tasks';
-  const HOST='mysql93.conoha.ne.jp';
-  const UTF='utf8mb4';
-  const USER='rkmcl_tasks';
-  const PASS='task&2525';
-  //データベースに接続する関数
-  public function pdo(){
-    /*phpのバージョンが5.3.6よりも古い場合はcharset=".self::UTFが必要無くなり、array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES '.SELF::UTF')が必要になり、5.3.6以上の場合は必要ないがcharset=".self::UTFは必要になる。*/
-    $dsn="mysql:dbname=".self::DB_NAME.";host=".self::HOST.";charset=".self::UTF;
-    $user=self::USER;
-    $pass=self::PASS;
-    try{
-      $pdo=new PDO($dsn,$user,$pass,array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES '.SELF::UTF));
-    }catch(Exception $e){
-      echo 'error' .$e->getMesseage;
-      die();
-    }
-    //エラーを表示してくれる。
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-    return $pdo;
+// データベースに登録する処理
+require_once('../db/dbconnection.php');
+
+class Project
+{
+  // データベースに接続する処理
+  private ?PDO $db;
+
+  public function __construct(){
+    $this->db = Database::dbConnect();
   }
-  //SELECT文のときに使用する関数。
-//   public function select($sql){
-//     $hoge=$this->pdo();
-//     $stmt=$hoge->query($sql);
-//     $items=$stmt->fetchAll(PDO::FETCH_ASSOC);
-//     return $items;
-//   }
-  //SELECT,INSERT,UPDATE,DELETE文の時に使用する関数。
-  public function plural($sql,$id,$name,$project,$color){
-    $hoge=$this->pdo();
-    $stmt=$hoge->prepare($sql);
-    $stmt->execute(array(':id'=>$id,':name'=>$name,':description'=>$project,':color'=>$color));//sql文のVALUES等の値が?の場合は$itemでもいい。
-    return $stmt;
+
+
+  // projectsに登録する機能
+  public function store(int $userId, array $params)
+  {
+    $statement = $this->db->prepare('INSERT into projects set user_id=?, name=?, description=?, color_type=?, created=Now()');
+    $statement->execute(array(
+      $userId,
+      $params['name'],
+      $params['description'],
+      $params['color_type']
+    ));
+  }
+  // projectsから一覧を取り出すファンクション
+  public function getMyProjects(int $userId): array
+  {
+    $query = $this->db->prepare('SELECT * from projects where user_id=? order by id desc');
+    $query->execute(array($userId));
+    return $query->fetchALL(PDO::FETCH_ASSOC);
+  }
+
+  // show.phpに表示する名前などを取り出す処理
+  public function getMyProject(int $userId, int $projectId)
+  {
+    $query = $this->db->prepare('SELECT * from projects where id=? and user_id=?');
+    $query->execute(array(
+      $projectId,
+      $userId
+    ));
+    return $query->fetch();
   }
 }
 ?>
