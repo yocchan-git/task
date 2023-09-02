71baa81356289994353be7ee8ca487d77258003c
yoshi-gid <yoshipanman1201@gmail.com>
2023-09-02 13:54:23 +0900

    タスク関連の処理クラス

--- project/task/Task.php
+++ project/task/Task.php
@@ -1,59 +1,80 @@
 <?php
+require_once(__DIR__ . '/../../db/dbconnection.php');
 
-class connect {
-  //定数の宣言
-  const DB_NAME='rkmcl_tasks';
-  const HOST='mysql93.conoha.ne.jp';
-  const UTF='utf8mb4';
-  const USER='rkmcl_tasks';
-  const PASS='task&2525';
-  //データベースに接続する関数
-  public function pdo(){
-    /*phpのバージョンが5.3.6よりも古い場合はcharset=".self::UTFが必要無くなり、array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES '.SELF::UTF')が必要になり、5.3.6以上の場合は必要ないがcharset=".self::UTFは必要になる。*/
-    $dsn="mysql:dbname=".self::DB_NAME.";host=".self::HOST.";charset=".self::UTF;
-    $user=self::USER;
-    $pass=self::PASS;
-    try{
-      $pdo=new PDO($dsn,$user,$pass,array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES '.SELF::UTF));
-    }catch(Exception $e){
-      echo 'error' .$e->getMesseage;
-      die();
+class Task {
+  const STATUS_NOT = 1;
+  const STATUS_DO = 2;
+  const STATUS_FIN = 3;
+
+  private ?PDO $db;
+
+  public function __construct(){
+    $this->db = Database::dbConnect();
+  }
+
+  // タスクの一覧を取得するコード
+  public function getTasks(int $projectId, int $status): array
+    {
+        $query = $this->db->prepare('SELECT * FROM tasks WHERE project_id=? AND status=? ORDER BY order_num');
+        $query->execute(array($projectId, $status));
+        return $query->fetchALL(PDO::FETCH_ASSOC);
     }
-    //エラーを表示してくれる。
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-    return $pdo;
-  }
-  //SELECT文のときに使用する関数。
-  public function select($sql){
-    $hoge=$this->pdo();
-    $stmt=$hoge->query($sql);
-    $items=$stmt->fetchAll(PDO::FETCH_ASSOC);
-    return $items;
-  }
-  //SELECT,INSERT,UPDATE文の時に使用する関数。
-  public function plural($sql,$item,$title,$description,$order_num,$status){
-    $hoge=$this->pdo();
-    $stmt=$hoge->prepare($sql);
-    $stmt->execute(array(':id'=>$item,':title'=>$title,':description'=>$description,':order_num'=>$order_num,':status'=>$status));//sql文のVALUES等の値が?の場合は$itemでもいい。
-    return $stmt;
-    // return $hoge -> lastInsertId();
-    // return $this->pdo()->lastInsertId();
+
+  // タスクを登録する処理
+  public function store(array $params): int
+  {
+    // order_numの値を決めるファンクションをよび、変数に代入
+    $newTaskOrderNumber = $this->getNewTaskOrderNum($params['project_id']);
+    // prepare文を用意する
+    $statement = $this->db->prepare('INSERT into tasks set project_id=?, title=?, description=?, order_num=?, status=?, created_at=Now()');
+    // executeで登録する（STATUSは１で登録）
+    $statement->execute(array(
+      $params['project_id'],
+      $params['title'],
+      $params['description'],
+      $newTaskOrderNumber,
+      self::STATUS_NOT
+    ));
+    // lastInsertIdで最後のIdをruturnする
+    return $this->db->lastInsertId();
   }
 
-  public function getId($sql,$title){
-    $hoge=$this->pdo();
-    $stmt=$hoge->prepare($sql);
-    $stmt->execute(array(':title'=>$title));
-    $get_id=$stmt->fetchAll(PDO::FETCH_ASSOC);
-    return $get_id;
+  // order_numを決める処理をする
+  private function getNewTaskOrderNum(int $projectId): int
+  {
+    // マックスの値を取得する
+    $query = $this->db->prepare('SELECT max(order_num) from tasks where project_id=? and status=1');
+    $query->execute(array($projectId));
+    $tasks = $query->fetch();
+    // nullなら１をnullでないなら+1以外の処理を入れる
+    if(!empty($tasks)){
+      return $tasks[0] + 1;
+    }else{
+      return 1;
+    }
   }
 
-  // delete文の時に使用する関数
-  public function delete($sql,$item){
-    $hoge=$this->pdo();
-    $stmt=$hoge->prepare($sql);
-    $stmt->execute(array(':id'=>$item));//sql文のVALUES等の値が?の場合は$itemでもいい。
-    return $stmt;
+  // タスクの更新処理
+  public function update(array $params)
+  {
+    $taskIds = explode(',',$params['taskIdList']);
+    foreach($taskIds as $key => $taskId){
+      $statement = $this->db->prepare('UPDATE tasks set order_num=?,status=?,updated_at=Now() where id=?');
+      $statement->execute(array(
+        $key + 1,
+        $params['status'],
+        $taskId
+      ));
+    }
   }
+  
+  // タスクの削除処理
+  public function delete(int $taskId)
+  {
+    $query = $this->db->prepare('DELETE from tasks where id=?');
+    $query->execute(array($taskId));
+  }
+
 }
 ?>
+
